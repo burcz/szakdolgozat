817861b4c0d646acff4d16a0dd93d41fde589273
Szabo Marton <burczeckley@gmail.com>
2019-10-27 13:50:17 +0100

    working test react UI

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,15 @@ WORKDIR /usr/app
 # Only copy the package.json file to work directory
 COPY package.json .
 COPY tsconfig.json .
+COPY webpack.config.js .
 # Install all Packages
 RUN npm install
 # Copy all other source code to work directory
 ADD ./src /usr/app/src
+ADD ./public /usr/app/public
 # TypeScript
 RUN node_modules/.bin/tsc -p tsconfig.json
+RUN node_modules/.bin/webpack
 # Start
 CMD [ "node", "dist/index.js" ]
 EXPOSE 8080
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as path from 'path';
 import routes from './routes';
 import connect from './connect';
 const app = express();
@@ -8,8 +9,16 @@ const port = 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+// Template configuration
+app.set("view engine", "ejs");
+app.set("views", "public");
+
+// Static files configuration
+app.use("/assets", express.static(path.join(__dirname, "views")));
+app.use('/favicon.ico', express.static(path.join(__dirname, '../public/favicon.ico')));
+
 app.get('/', (req: express.Request, res: express.Response) =>
-	res.send('Welcome to the Mongoose & TypeScript example')
+	res.render("index")
 );
 
 app.listen(port, () =>
